f469eedd7f996cd457605491c017a2eb98e5fbb0
Diamond-Programmer <87809313+Diamond-Programmer@users.noreply.github.com>
2021-11-15 17:53:06 +0500

    newPortfolio

---
+++ src/page/index.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import "./style.css"
+function Page() {
+    return (
+        <div>
+        <div class="banner">
+         <div class="panel"></div>
+         <div class="panel"></div>
+         <div class="panel"></div>
+         <div class="panel"></div>
+         <div class="panel"></div>
+          <div class="panel"></div>
+          <div class="panel"></div>
+          <div class="panel"></div>
+          <div class="panel"></div>
+          <div class="panel"></div>
+          <div class="panel"></div>
+          <div class="panel"></div>
+          <div class="panel"></div>
+          <div class="panel"></div>
+          <div class="panel"></div>
+          <div class="panel"></div>
+          <div class="panel"></div>
+          <div class="panel"></div>
+          <div class="panel"></div>
+          <div class="panel"></div>
+          <div class="panel"></div>
+          <div class="panel"></div>
+          <div class="panel"></div>
+          <div class="panel"></div>
+        </div>
+        </div>
+    )
+}
+
+export default Page
